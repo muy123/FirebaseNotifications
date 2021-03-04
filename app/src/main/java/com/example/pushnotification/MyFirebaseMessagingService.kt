712d935e5ef0141d644d39fa387717700e3785e6
Ansul Singh <ansulsingh@shubham-bhaskar.local>
2021-03-04 16:01:02 +0530

    pending intent

--- app/src/main/java/com/example/pushnotification/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/pushnotification/MyFirebaseMessagingService.kt
@@ -1,7 +1,7 @@
 package com.example.pushnotification
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -12,20 +12,30 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
 
     override fun onMessageReceived(p0: RemoteMessage) {
         super.onMessageReceived(p0)
-        getFirebaseMessage(p0.notification?.title,p0.notification?.body)
+        getFirebaseMessage(p0.notification?.title, p0.notification?.body)
     }
 
     private fun getFirebaseMessage(title: String?, body: String?) {
+        //intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+
+
+        val myIntent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            myIntent,0)
+
         //builder ki help se message ready kiya hmne
         var builder = NotificationCompat.Builder(this, "FirebaseChannel")
             .setSmallIcon(R.drawable.ic_baseline_notifications_24)
             .setContentTitle(title)
             .setContentText(body)
+            .setContentIntent(pendingIntent)
             .setAutoCancel(true)
 
         //manager ki help se hmne use notify kar diya mtlb print kar diya
         var manager=NotificationManagerCompat.from(this)
-        manager.notify(101,builder.build())
+        manager.notify(101, builder.build())
 
 
     }
